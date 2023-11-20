b50e576b4a36412c216de3009d027e6d14b984b9
extramix <sirapat.jr@gmail.com>
2023-11-20 14:03:13 +0700

    click top-left logo will now naviagate to dashboard when logged in

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,16 +1,25 @@
 import Link from 'next/link';
 import MaxWidthWrapper from '../MaxWidthWrapper';
-import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from '@kinde-oss/kinde-auth-nextjs/server';
 import { buttonVariants } from './button';
 import { ArrowRight } from 'lucide-react';
 
 const Navbar = () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
   return (
     <>
       <nav className='sitcky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
         <MaxWidthWrapper>
           <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
-            <Link href='/' className='flex z-40 font-semibold'>
+            <Link
+              href={!user ? '/' : '/dashboard'}
+              className='flex z-40 font-semibold'
+            >
               <span>Niche</span>
             </Link>
 
